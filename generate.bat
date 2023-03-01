562faef74f66a8139f9513812dd0e4448ce26335
ustclyh <50285760+ustclyh@users.noreply.github.com>
2023-03-01 20:37:46 +0800

    update interests

--- generate.bat
+++ generate.bat
@@ -1,3 +1,2 @@
 python jemdoc.py -c mysite.conf index.jemdoc
 python jemdoc.py -c mysite.conf publications.jemdoc
-python jemdoc.py -c mysite.conf interests.jemdoc
---
+++ main.py
@@ -0,0 +1,4 @@
+import os
+
+os.system('python jemdoc.py -c mysite.conf index.jemdoc')
+os.system('python jemdoc.py -c mysite.conf publications.jemdoc')
